0fa9122972bbd5f13fd8b9cf6bc64baf4bc48fbc
Rinchin Angarkhaev <rin4in.2014@yandex.ru>
2020-12-19 12:07:14 +0300

    feat: вывод карточек стихов

--- index.js
+++ index.js
@@ -1,14 +1,17 @@
 const containerOfPoems = document.querySelector('.poems-container')
 const templatePoem = document.querySelector('#template-poem')
 const form = document.querySelector('.form')
-// Проверка на наличие ОДНОГО слова
+const checkPoem = document.querySelector('.poem')
+
+// Поиск до 4 слов и вывод всех строк со словами
 function searchPoems(setOfPoems, word1, word2 = "", word3 = "", word4 = "") {
   const filterObj = setOfPoems.filter((item) => {
     return item.fields.text.includes(word1) && item.fields.text.includes(word2) && item.fields.text.includes(word3) && item.fields.text.includes(word4)
   })
   const result = filterObj.map((item) => {
-    //   let regex = new RegExp("(.+(" + word1 + "|" + word2 + "|" + word3 + "|" + word4 + ").+)", "gm");
-    //   let m = regex.exec(item.fields.text)
+    // неудачные регулярки...
+    // let regex = new RegExp("(.+(" + word1 + "|" + word2 + "|" + word3 + "|" + word4 + ").+)", "gm");
+    // let m = regex.exec(item.fields.text)
     let stringToArr = item.fields.text.split('\n')
     let poemSearchRow = stringToArr.map((row) => {
       let rowPoem = ""
@@ -36,6 +39,7 @@ function searchPoems(setOfPoems, word1, word2 = "", word3 = "", word4 = "") {
 const requestURL = 'http://buymebuyme.xyz'
 const xhr = new XMLHttpRequest()
 xhr.responseType = 'json'
+
 //Запрос и вывод стихов
 function outputPoem(evt) {
   evt.preventDefault()
@@ -45,24 +49,25 @@ function outputPoem(evt) {
       console.error('ошибка')
     } else {
       let [word1, word2, word3, word4] = document.querySelector('.form__input').value.split(' ')
-      console.log(searchPoems(xhr.response, word1, word2, word3, word4))
+      renderPoemsList(searchPoems(xhr.response, word1, word2, word3, word4), containerOfPoems)
     }
   }
   xhr.onerror = () => { console.log('ошибка') }
   xhr.send()
 }
 
-// При нажатии на кнопку формы
-form.addEventListener('submit', outputPoem)
-
-function createPoem() {
+// Создание карточки стиха
+function createPoemCard(poemLine) {
   const poem = templatePoem.content.cloneNode(true)
   const poemText = poem.querySelector('.poem__text')
-  poemText.textContent = searchPoems(searchWord1, xhr.response)
+  poemText.textContent = poemLine
   return poem
 }
-
-function addPoemsInContainer(arrOfPoems, containerOfPoems) {
-  const newPoem = createPoem()
-  newPoem.append
+// Вывод карточек стихов
+function renderPoemsList(poemArr, containerToWritePoems) {
+  const list = poemArr.map(createPoemCard)
+  containerToWritePoems.append(...list)
 }
+
+// При нажатии на кнопку формы поиск и вывод стихов
+form.addEventListener('submit', outputPoem)
